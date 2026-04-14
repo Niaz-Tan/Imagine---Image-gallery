33ffcb186e08a16927db8a1cb162a6d4f51b5c1f
Niaz-Tan <nuddintanim@gmail.com>
2026-04-14 22:18:28 +0600

    fix: removing env again, and fixing fetch url

--- components/PhotoList.js
+++ components/PhotoList.js
@@ -5,7 +5,7 @@ import Link from "next/link";
 import { CiHeart } from "react-icons/ci";
 
 const PhotoList = async () => {
-  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/photos`, {
+  const res = await fetch(`https://iig-one.vercel.app/api/photos`, {
     cache: "no-store",
   });
 
