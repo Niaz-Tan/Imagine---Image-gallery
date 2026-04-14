07546816eccc11614df4a81a9251136b6c18f34b
Niaz-Tan <nuddintanim@gmail.com>
2026-04-14 21:34:34 +0600

    feat: improved UI

--- components/Header.js
+++ components/Header.js
@@ -1,15 +1,20 @@
+import { FaCode } from "react-icons/fa";
+
 const Header = () => {
   return (
     <div className="px-20 py-2 border-b border-zinc-800 flex justify-between items-center">
       <div className="text-4xl font-bold italic select-none">IIG</div>
-      <p className="bg-yellow-200 px-5 py-2 text-red-500 text-lg font-semibold">
+      <p className="text-red-500 text-3xl font-semibold">
         Under Construction
       </p>
       <a
         href="https://github.com/Niaz-Tan/Imagine---Image-gallery"
-        className="bg-zinc-50 text-zinc-950 px-5 py-1 rounded font-semibold select-none cursor-pointer hover:bg-zinc-400 active:scale-95"
+        className="bg-zinc-50 text-zinc-950 px-5 py-2 rounded font-semibold select-none cursor-pointer hover:bg-zinc-400 active:scale-95 flex justify-center items-center gap-2"
         target="_blank"
       >
+        <div className="text-xl">
+          <FaCode />
+        </div>
         Source code
       </a>
     </div>
--- components/PhotoList.js
+++ components/PhotoList.js
@@ -1,3 +1,7 @@
+// import { getFollowers } from "@/lib/methods";
+import { CiHeart } from "react-icons/ci";
+
+import { formate } from "@/lib/methods";
 import Image from "next/image";
 import Link from "next/link";
 
@@ -12,29 +16,56 @@ const PhotoList = async () => {
 
   const photos = await res.json();
 
+  // Handlers
+
   return (
     <div className="p-4">
       <div className="columns-1 sm:columns-2 md:columns-3 lg:columns-4 gap-4 space-y-4">
         {photos.map((photo) => (
-          <Link
+          <div
             key={photo.id}
-            href={`/photos/${photo.id}`}
-            className="block break-inside-avoid overflow-hidden rounded-2xl bg-white shadow-md hover:shadow-xl transition duration-300 hover:scale-[1.02]"
+            className="block break-inside-avoid overflow-hidden rounded-2xl bg-zinc-900 transition duration-300 hover:scale-[1.02] border border-zinc-800"
           >
-            <Image
-              src={photo.url}
-              alt={photo.title || "photo"}
-              width={700}
-              height={700}
-              className="w-full object-cover"
-            />
-
-            {photo.title && (
-              <div className="p-3 text-sm text-gray-700">
-                {photo.title}
+            <Link href={`/photos/${photo.id}`}>
+              <div>
+                <Image
+                  src={photo.url}
+                  alt={photo.title || "photo"}
+                  width={700}
+                  height={700}
+                  className="w-full object-cover"
+                />
               </div>
-            )}
-          </Link>
+            </Link>
+            <div className="px-5 py-2">
+              <div className="flex justify-between items-center">
+                <h1 className="text-lg font-semibold w-full">{photo.title}</h1>
+                <button className="flex items-center justify-center gap-1 border border-zinc-800 px-2 py-1 rounded-xl cursor-pointer">
+                  <div className="text-xl">
+                    <CiHeart />
+                  </div>
+                  <div className="text-sm">{formate(photo.id, "likes")}</div>
+                </button>
+              </div>
+              {/* <div className="flex items-center gap-2">
+                <div className="">
+                  <Image
+                    src={photo.author.avatar}
+                    alt={photo.title || "photo"}
+                    width={40}
+                    height={40}
+                    className="rounded-full"
+                  />
+                </div>
+                <div className="">
+                  <div className="text-sm text-zinc-200">{photo.title}</div>
+                  <div className="text-xs text-zinc-400">
+                    {getFollowers(photo.id)} followers
+                  </div>
+                </div>
+              </div> */}
+            </div>
+          </div>
         ))}
       </div>
     </div>
