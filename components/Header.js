874afcbb3deb816546722a81bb4dae01981d7d77
Niaz-Tan <nuddintanim@gmail.com>
2026-04-14 21:45:39 +0600

    fix: fixed env

--- components/Header.js
+++ components/Header.js
@@ -4,9 +4,7 @@ const Header = () => {
   return (
     <div className="px-20 py-2 border-b border-zinc-800 flex justify-between items-center">
       <div className="text-4xl font-bold italic select-none">IIG</div>
-      <p className="text-red-500 text-3xl font-semibold">
-        Under Construction
-      </p>
+      <p className="text-red-500 text-3xl font-semibold">Under Construction</p>
       <a
         href="https://github.com/Niaz-Tan/Imagine---Image-gallery"
         className="bg-zinc-50 text-zinc-950 px-5 py-2 rounded font-semibold select-none cursor-pointer hover:bg-zinc-400 active:scale-95 flex justify-center items-center gap-2"
